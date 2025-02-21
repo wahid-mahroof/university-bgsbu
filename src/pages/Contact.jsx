391907c2520fa93cd7c9a0e4b28750e9f77044a3
wahid-mahroof <wahidmahroof040@gmail.com>
2025-02-21 06:28:06 -0800

    update again

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -14,35 +14,35 @@ export function Contact() {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ duration: 0.5 }}
-      className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-r from-gray-200 dark:bg-gray-900 text-white p-6"
+      className="flex flex-col max-w-auto items-center justify-center  bg-gradient-to-r from-gray-200 dark:bg-gray-900 text-white p-6 "
     >
       <h1 className="text-5xl font-extrabold mb-6">Contact Us</h1>
       <p className="text-lg mb-8">
         Follow us on social media and stay updated with our latest news.
       </p>
-      <div className="flex space-x-8 text-4xl flex-col gap-5">
-        <a href="#" className="hover:text-blue-400 transition">
+      <div className="flex space-x-8 text-4xl flex-col gap-5  ">
+        <a href="#" className="hover:text-blue-400 transition ml-20 ">
           <FaFacebook />
-          <span className="flex text-sm">bgsbu offical</span>
+          <span className="text-sm mr-20">bgsbu offical</span>
         </a>
-        <a href="#" className="hover:text-blue-300 transition">
+        <a href="#" className="hover:text-blue-300 transition ml-20">
           <FaTwitter />
-          <span className="flex text-sm">
+          <span className="flex items-center justify-center text-sm">
             https://twitter.com/bgsbuofficial?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor
           </span>
         </a>
 
-        <a href="#" className="hover:text-pink-400 transition">
+        <a href="#" className="hover:text-pink-400 transition ml-20">
           <FaInstagram />
-          <span className="flex text-sm">
+          <span className=" text-sm">
             https://www.instagram.com/bgsbuniversity_official/
           </span>
         </a>
-        <a href="#" className="hover:text-pink-400 transition">
+        <a href="#" className="hover:text-pink-400 transition ml-20">
           <FaMailBulk />{" "}
-          <span className="flex text-sm"> university@bgsbu.ac.in</span>
+          <span className=" text-sm"> university@bgsbu.ac.in</span>
         </a>
-        <a href="#" className="hover:text-pink-400 transition">
+        <a href="#" className="hover:text-pink-400 transition ml-20">
           <FaPhone /> <span className="flex text-sm"> +91-1962-241002</span>
         </a>
       </div>
