d9f8e0c78be260b4ffd52ade791600f8c308b9f1
wahid-mahroof <wahidmahroof040@gmail.com>
2025-02-21 03:45:33 -0800

    change path

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Contact from "./pages/Contact";
 import DepartmentDetails from "./pages/DepartmentDetails";
-import University from "./pages/university";
+import University from "../pages/University";
 
 function App() {
   return (
